ab4fda9e680b34ec10a889b609df519486ff99b9
yeliex <yeliex@yeliex.com>
2016-11-07 17:00:36 +0800

    fix post data support

--- src/index.browser.js
+++ src/index.browser.js
@@ -1,11 +1,21 @@
 const realFetch = require('isomorphic-fetch/fetch-npm-browserify.js');
-const { responseMiddleware, parseUrl } = require('./utils');
+const { responseMiddleware, parseUrl, toFormData } = require('./utils');
 
 let globalCallback = response => response;
 let baseHost = '';
 
 const fetchRequest = (url, options = {}, ...extras) => {
-  options.data ? options.method === 'GET' ? options.query = {...(options.query || {}), ...options.data} : options.body = {...(options.body || {}), ...options.data} : '';
+  options.data ? options.method === 'GET' ? options.query = { ...(options.query || {}), ...options.data } : options.body = { ...(options.body || {}), ...options.data } : '';
+
+  if (!options.method || options.method === 'GET') {
+    options.body = options.body || options.data;
+  }
+
+  options.headers = options.headers || {};
+  if (options.headers['Content-Type'] !== 'application/json' && !(options.body instanceof 'FormData')) {
+    // convert to FormData
+    options.body = toFormData(body);
+  }
 
   return realFetch.call(this, parseUrl(url, options, baseHost), options, ...extras).then(responseMiddleware).then(globalCallback);
 };
--- src/utils.js
+++ src/utils.js
@@ -20,7 +20,17 @@ const parseUrl = (url, { ssl, method, body, query }, baseHost) => {
   return url;
 };
 
+const toFormData = (obj) => {
+  const form = new FormData();
+  Object.keys(obj).forEach((key)=> {
+    form.append(key, obj[key]);
+  });
+
+  return form;
+}
+
 module.exports = {
   parseUrl,
-  responseMiddleware
+  responseMiddleware,
+  toFormData
 };
