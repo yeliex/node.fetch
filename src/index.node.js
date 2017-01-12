b23067f03943e9ac76f01110c6bb9c86e7f1ba15
yeliex <yeliex@yeliex.com>
2017-01-12 17:28:26 +0800

    添加对node环境下的Headers.keys,Headers.values支持

--- src/index.node.js
+++ src/index.node.js
@@ -1,6 +1,17 @@
 "use strict";
 
 const realFetch = require('isomorphic-fetch/fetch-npm-node.js');
+
+global.Headers.prototype.keys = global.Headers.prototype.keys ||
+  function () {
+    return Object.keys(this._headers || {});
+  };
+
+global.Headers.prototype.values = global.Headers.prototype.values ||
+  function () {
+    return Object.keys(this._headers || {}).map((key) => this._headers[k]);
+  };
+
 const { fetchDecorator } = require('./../dist/utils');
 
 const fetch = fetchDecorator(realFetch);
