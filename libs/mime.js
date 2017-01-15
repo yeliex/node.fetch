787200bb553036edbe6788f13e84a6ef2683a875
yeliex <yeliex@yeliex.com>
2017-01-16 01:20:44 +0800

    fix FormData is undefined error in IE8

--- libs/mime.js
+++ libs/mime.js
@@ -2,7 +2,7 @@ const types = require('./mime-types');
 
 // detect mime type with data
 const mime = (data, json) => {
-  if (typeof window !== 'undefined' && data instanceof FormData) {
+  if (typeof window !== 'undefined' && typeof FormData !== 'undefined' && data instanceof FormData) {
     return types.formData;
   }
   if (typeof data === 'string') {
